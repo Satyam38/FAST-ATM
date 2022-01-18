b462819b61fb8d8d5c1ec8358bae5aaf5b6e4dd0
Coder Sathu <89405234+Satyam868910@users.noreply.github.com>
2022-01-18 21:17:47 +0530

    Add files via upload

---
+++ Fast atm in c++/Atm.cpp
@@ -0,0 +1,167 @@
+
+//-----------------------------------------------------------> CODER SATHU <------------------------------------------------------------
+
+
+
+#include<iostream>
+#include<windows.h>
+#include<time.h>
+using namespace std;
+
+unsigned long amount=1000, deposit, withdraw;
+
+int choice, pin, k;
+
+char transaction ='y';
+
+int main()
+{
+
+
+cout<< "\n \n \n \n \n "<<endl;
+cout<< "\t \t \t \t \t "<< endl;
+for (int i=0; i<120; i++){
+    cout<<"_";
+    Sleep(15);
+
+
+}
+
+time_t now;
+time(&now);
+printf("\n");
+printf("%s",ctime(&now));
+
+
+	while (pin != 1234)
+
+	{
++
+		printf("\n\nENTER YOUR SECRET PIN NUMBER:");
+
+		scanf("%d", &pin);
+        Beep( 610, 500);
+
+		if (pin != 1234)
+
+		printf("\n \nPLEASE ENTER VALID PASSWORD\n\n\n");
+
+	}
+
+	do
+
+	{
+  
+
+		printf("\t\t\t-----------------> Welcome to ATM Service  <--------------------\n");
+
+		printf("1- Check Balance\n");
+
+		printf("2- Withdraw Cash\n");
+
+		printf("3- Deposit Cash\n");
+
+		printf("4- Quit\n");
+
+		printf("-------------------------------------------\n\n");
+
+		printf("Enter your choice: ");
+
+		scanf("%d", &choice);
+
+		switch (choice)
+
+		{
+
+		case 1:
+
+			printf("\n YOUR BALANCE IN Rs : %lu ", amount);
+
+			break;
+
+		case 2:
+
+			printf("\n ENTER THE AMOUNT TO WITHDRAW: ");
+
+			scanf("%lu", &withdraw);
+
+			if (withdraw % 100 != 0)
+
+			{
+
+				printf("\n PLEASE ENTER THE AMOUNT IN MULTIPLES OF 100");
+
+			}
+
+			else if (withdraw >(amount - 500))
+
+			{
+
+				printf("\n INSUFFICENT BALANCE");
+
+			}
+
+			else
+
+			{
+
+				amount = amount - withdraw;
+
+				printf("\n\n PLEASE COLLECT CASH");
+
+				printf("\n YOUR CURRENT BALANCE IS  %lu:", amount);
+
+			}
+
+			break;
+
+		case 3:
+
+			printf("\n ENTER THE AMOUNT TO DEPOSIT");
+
+			scanf("%lu", &deposit);
+
+amount = amount + deposit;
+
+			printf("YOUR BALANCE IS %lu:", amount);
+
+			break;
+
+		case 4:
+
+			printf("\n THANK U USING ATM");
+
+			break;
+
+		default:
+
+			printf("\n INVALID CHOICE");
+
+		}
+
+		printf("\n\n\n DO U WISH TO HAVE ANOTHER TRANSCATION?( Yes or No): \n");
+
+		fflush(stdin);
+
+		scanf("%c", &transaction);
+
+		if (transaction == 'n'|| transaction == 'N')
+
+k = 1;
+
+	} while (!k);
+
+	printf("\n\n THANKS FOR USING OUT ATM SERVICE");
+
+ return 0;
+
+
+
+
+}
+
+
+
+
+//-----------------------------------------------------------> CODER SATHU <------------------------------------------------------------
+
